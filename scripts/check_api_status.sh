e893c63ccc74d9b8a57e06cfcaee3c7d6eb20160
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-09-18 12:43:53 +0200

    Added health checks

--- scripts/check_api_status.sh
+++ scripts/check_api_status.sh
@@ -2,6 +2,8 @@ echo "🚀 Health checks"
 
 ./scripts/wait_for_url.sh \
     "http://localhost:8080/api/info/version" \
-    "http://localhost:8080/api/metadatablocks"
+    "http://localhost:8080/api/metadatablocks" \
+    "http://localhost:8080/api/metadatablocks/citation" \
+    "http://localhost:8080/api/dataverses/root"
 
 echo -e "✅ Health checks done\n"
