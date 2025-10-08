d600d4f007e5ba4395bd4cb6784bffb2437ae3af
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-10-08 09:52:07 +0200

    Add log collection for modified compose file
    Collects logs from the 'dataverse' service using the modified docker-compose file and saves them to 'modified-docker-compose.yml' in the artifacts directory.

--- src/post.js
+++ src/post.js
@@ -114,6 +114,11 @@ async function collectDataverseLogs(config, artifactsDir, additionalServices = [
     logFiles.push(smtpLogFile);
     await collectComposeServiceLogs(config, smtpLogFile, 'smtp');
 
+    // Modified compose file
+    const modifiedComposeFile = path.join(artifactsDir, 'modified-docker-compose.yml');
+    logFiles.push(modifiedComposeFile);
+    await collectComposeServiceLogs(config, modifiedComposeFile, 'dataverse');
+
     // Collect logs from additional services
     if (additionalServices.includes('localstack')) {
         const localstackLogFile = path.join(artifactsDir, 'localstack.log');
